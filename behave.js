7f8b9bbc9e2d0e9b217ad0d13df5946f78dc000e
Teddy Georgia Waggy <teddygeorgiawaggy@gmail.com>
2015-03-04 15:22:33 -0600

    First draft of D. Markham's site
    Includes HTML, Less and JS files for site, plus placeholder images
    while waiting on client content

---
+++ behave.js
@@ -0,0 +1,15 @@
+//Smoothly scroll between sections on clicking nav links
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1500);
+        return false;
+      }
+    }
+  });
+});
